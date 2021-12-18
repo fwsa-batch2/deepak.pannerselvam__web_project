fa2839e6b7aba775056b65301745b36f0578cab1
Deepakpanner <deepakthirup@gmail.com>
2021-12-18 13:56:45 +0530

    modified the file

--- assets/java/review.js
+++ assets/java/review.js
@@ -1,9 +1,14 @@
 const btn= document.querySelector("button");
 const post= document.querySelector(".post");
 const widget = document.querySelector(".star-widget");
-const exitBtn = document.querySelector(".edit");
+const editBtn = document.querySelector(".edit");
 btn.onclick = ()=>{
     widget.style.display = "none";
     post.style.display = "block";
     return false;
+editBtn.onclick = ()=>{
+    widget.style.display = "block";
+    post.style.display = "none"
+}
+return false;
 }
